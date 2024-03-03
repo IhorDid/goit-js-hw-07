55919879d9ad70d55eebb8dd18b2e9b93de0e64a
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-03-03 17:34:13 +0200

    upfix

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,22 @@
-const categoriesItems = document.querySelectorAll('li.item');
-console.log('Number of categories:', categoriesItems.length);
-categoriesItems.forEach(categoriesItem => {
-  const categoryTitle = categoriesItem.querySelector('h2').textContent;
-  const categoryItem = categoriesItem.querySelectorAll('ul > li').length;
-  console.log(`Category: ${categoryTitle}`);
-  console.log(`Elements: ${categoryItem}`);
-});
+// const categoriesItems = document.querySelectorAll('li.item');
+// console.log('Number of categories:', categoriesItems.length);
+// categoriesItems.forEach(categoriesItem => {
+//   const categoryTitle = categoriesItem.querySelector('h2').textContent;
+//   const categoryItem = categoriesItem.querySelectorAll('ul > li').length;
+//   console.log(`Category: ${categoryTitle}`);
+//   console.log(`Elements: ${categoryItem}`);
+// });
+const itemEl = document.querySelector('#categories');
+const itemElTotal = itemEl.children;
+console.log('Number of categories:', itemElTotal.length);
+const linksEl = document.querySelectorAll('.item');
+for (const linkEl of linksEl) {
+  console.log('Category:', linkEl.firstElementChild.textContent);
+  const linksItemEl = linkEl
+    .querySelectorAll('ul')
+    .forEach(link => console.log('Elements:', link.children.length));
+
+  // console.log(linksItemEl);
+
+  // console.log('Elements:', linksItemEl.children.length);
+}
--- js/task-2.js
+++ js/task-2.js
@@ -1,38 +1,65 @@
+// const images = [
+//   {
+//     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'White and Black Long Fur Cat',
+//   },
+//   {
+//     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+//   },
+//   {
+//     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'Group of Horses Running',
+//   },
+//   {
+//     url: 'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+//     alt: 'Elephant Beside on Baby Elephant',
+//   },
+//   {
+//     url: 'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+//     alt: 'Blue Geeen and Orange Parrot',
+//   },
+//   {
+//     url: 'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+//     alt: 'Zebras on Zebra',
+//   },
+// ];
+// const galleryAll = document.querySelector('.gallery');
+// let galleryItemsHTML = ' ';
+
+// images.forEach(({ url, alt }) => {
+//   galleryItemsHTML += `
+//     <li>
+//       <img  class="galery-img" src="${url}" alt="${alt}" width="360" height="300">
+//     </li>
+//   `;
+// });
+
+// galleryAll.insertAdjacentHTML('afterbegin', galleryItemsHTML);
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260',
     alt: 'White and Black Long Fur Cat',
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260',
     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
-  {
-    url: 'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Elephant Beside on Baby Elephant',
-  },
-  {
-    url: 'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Blue Geeen and Orange Parrot',
-  },
-  {
-    url: 'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Zebras on Zebra',
-  },
 ];
-const galleryAll = document.querySelector('.gallery');
-let galleryItemsHTML = ' ';
 
-images.forEach(({ url, alt }) => {
-  galleryItemsHTML += `
-    <li>
-      <img  class="galery-img" src="${url}" alt="${alt}" width="360" height="300">
-    </li>
-  `;
+const galeryEl = document.querySelector('.gallery');
+const imagesEl = images.map(image => {
+  const linkEl = document.createElement('li');
+  const itemEL = document.createElement('a');
+  linkEl.append(itemEL);
+  const imgEl = document.createElement('img');
+  itemEL.append(imgEl);
+  imgEl.src = image.url;
+  imgEl.alt = image.alt;
+  return linkEl;
 });
-
-galleryAll.insertAdjacentHTML('afterbegin', galleryItemsHTML);
+galeryEl.append(...imagesEl);
